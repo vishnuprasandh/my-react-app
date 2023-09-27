2e9b07ea9a77e01a2917b8a0fa2fb8a7f92e7c4f
vishnuprasandh <vishnuprasandh2002@gmail.com>
2023-09-27 19:45:42 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./comonents/auth/LoginPage";
-import RegisterPage from "./comonents/auth/RegisterPage";
+// import RegisterPage from "./comonents/auth/RegisterPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={< RegisterPage/>}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/register" element={< RegisterPage/>}></Route>
+    //     <Route path="/login" element={<LoginPage />}></Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+      <LoginPage/>
+    </div>
   );
 }
 
